7522fd577cc6de743dd58e110b229462c88b4319
Hui Lau <124365190+scshkl@users.noreply.github.com>
2026-02-16 14:30:21 +0000

    demo pointer array duality

--- session-1/task1/demo.c
+++ session-1/task1/demo.c
@@ -13,9 +13,31 @@ int main(void){
         ptr++;
     }
     */
+    /* ======================================================================== */
+    // dynamic array & duality with pointers
+    int *p = calloc(5, sizeof(int));
 
-    // dynamic array
-    int *p = calloc(8, sizeof(int));
+    // does it really initialises to 0 for int?  yes!
+    //for(int j=0; j<5; j++){
+    //    printf("%d\n", p[j]);
+    //}
+
+    // how about malloc? not always, depends on what's on memory
+    int *q = malloc(5*sizeof(int));
+   // for(int j=0; j<5; j++){
+    //    printf("%d\n", q[j]);
+   // }
+
+    // how about calloc for char? '\0' (0 in %d) - null character
+
+    char *cptr = calloc(5, sizeof(char));
+    //for(int j=0; j<5; j++){
+    //    printf("%d \n", cptr[j]);
+    //}
+
+    // how about float or double? It sets every bit in the allocated memory to zero, which is 0.0 in modern systems
+
+    /*
     p[0] = 1;
     for(int j=1; j<5; j++){
        // p[j] = p[j-1]+2;
@@ -28,10 +50,33 @@ int main(void){
         // if do not want access through index like array
         printf("%d\n", *(p+i));
     }
-
-
     
-   //printf("%d\n", vec[5]);  //vec[5], index 5 out of bound
+    */
 
+    // 2D array
+    int **table1, table2[4][5];
+
+    table1 = calloc(4, sizeof(int *));
+    for (int i=0; i<4; i++){
+        table1[i] = calloc(5, sizeof(int));
+        for(int j=0; j<5; j++){
+            table1[i][j] = i*j+10;
+            table2[i][j] = i*j+10;
+        }
+        printf("\n");
+    }
+    for (int i=0; i<4; i++){
+        for(int j=0; j<5; j++){
+            printf("%d ", table1[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (int i=0; i<4; i++){
+        for(int j=0; j<5; j++){
+            printf("%d ", table2[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
