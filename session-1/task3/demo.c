526a8a7b46750c6e6d31b30c9ef106af54dc2c5f
Hui Lau <124365190+scshkl@users.noreply.github.com>
2026-02-16 10:19:31 +0000

    demo

--- session-1/task3/demo.c
+++ session-1/task3/demo.c
@@ -2,14 +2,16 @@
 
  int main( void ) {
     char test[10] ={'1', '2', '3', '4', '5', '6', '7', '8', '9', 'a'};
+    int num[3]={12, 13, 1234};
+
     int *int_ptr;
     char *char_ptr;
 
-    char_ptr = test;
+    int_ptr = num;
 
-    for(int i=0; i<10; i++){
-        printf("char: %c \n", *char_ptr);
-        char_ptr++;
+    for(int i=0; i<3; i++){
+        printf("d: %d \n", *int_ptr);
+        int_ptr++;
     }
 
     return 0;
